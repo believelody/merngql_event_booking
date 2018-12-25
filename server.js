92b3952c88d4d4dd7ff4370694f53bce083d8159
believelody <believelody@gmail.com>
2018-12-25 17:09:03 +0100

    Implemented schema and resolver

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-app.get('/', (req, res, next) => {
-    res.send('Yes we can');
-})
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['test', 'test1', 'test2'];
+        },
+        createEvent: (args) => {
+            const { name } = args;
+            return name;
+        }
+    },
+    graphiql: true
+}));
 
 app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
