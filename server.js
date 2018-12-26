02205ba15161097e2485e7eabf79a1ec67da922b
believelody <believelody@gmail.com>
2018-12-26 12:54:40 +0100

    Implements models and add event feature

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+const Event = require('./models/event');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-const events = [];
 
 app.use(bodyParser.json());
 app.use('/graphql', graphqlHTTP({
@@ -16,7 +17,7 @@ app.use('/graphql', graphqlHTTP({
             title: String!
             description: String!
             price: Float!
-            date: String
+            date: String!
         }
 
         input EventInput {
@@ -41,23 +42,42 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         events: () => {
-            return events;
+            return Event
+                .find()
+                .then(events => events.map(event => ({ ...event._doc, _id: event.id })))
+                .catch(err => {
+                    console.error(err);
+                    throw err;
+                });
         },
         createEvent: ({eventInput}) => {
-            const { title, description, price } = eventInput;
-            const event = {
-                _id: Math.random().toString(),
+            const { title, description, price, date } = eventInput;
+            const event = new Event({
                 title,
                 description,
                 price: +price,
-                date: new Date().toISOString()
-            }
+                date: new Date(date)
+            });
 
-            events.push(event);
+            return event
+                .save()
+                .then(res => {
+                    return {...res._doc};
+                })
+                .catch(err => {
+                    console.error(err);
+                    throw err;
+                });
+
             return event;
         }
     },
     graphiql: true
 }));
 
-app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
+mongoose
+    .connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@merngql-event-booking-full5.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
+    .then(() =>{
+        app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`));
+    })
+    .catch(err => console.error(err));
